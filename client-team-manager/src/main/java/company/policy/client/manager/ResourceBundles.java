09b318fae6a8245453f1e2b0999b1c45b5c6c840
Hiram K <hiram.kamau@outlook.com>
2023-03-22 02:08:39 +0300

    Create ResourceBundles.java

---
+++ client-team-manager/src/main/java/company/policy/client/manager/ResourceBundles.java
@@ -0,0 +1,19 @@
+package company.policy.client.manager;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public enum ResourceBundles {
+    DEFAULT("i18n/messages", Locale.getDefault()), ITALIAN("i18n/messages", Locale.ITALIAN);
+
+    private final ResourceBundle resourceBundle;
+
+    private ResourceBundles(String baseName, Locale locale) {
+        resourceBundle = ResourceBundle.getBundle(baseName, locale);
+    }
+
+    public ResourceBundle getResourceBundle() {
+        return resourceBundle;
+    }
+
+}
