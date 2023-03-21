1911ab4e8a72fa2eaa78d4c06ca72051747ead14
Hiram K <hiram.kamau@outlook.com>
2023-03-22 02:07:56 +0300

    Update ChatClientThreadTest.java

--- client-core/src/test/java/company/policy/client/core/ChatClientThreadTest.java
+++ client-core/src/test/java/company/policy/client/core/ChatClientThreadTest.java
@@ -1,6 +1,7 @@
 package company.policy.client.core;
 
 import java.text.MessageFormat;
+import static java.text.MessageFormat.format;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import org.junit.Test;
@@ -12,6 +13,7 @@ public class ChatClientThreadTest {
     @Test
     public void testSomeMethod() {
         System.out.println(MessageFormat.format(BUNDLE.getString("error_getting_input_stream"), "hello"));
+        System.out.println(format(BUNDLE.getString("listening_error"), "blah blah"));
     }
 
 }
