f45ff4d47662f0b4a253cc8d21fad96b2887557c
Hiram K <hiram.kamau@outlook.com>
2023-03-22 14:43:57 +0300

    Update AnswersApplication.java

--- client-staff/src/main/java/company/policy/client/staff/AnswersApplication.java
+++ client-staff/src/main/java/company/policy/client/staff/AnswersApplication.java
@@ -19,8 +19,8 @@ import javafx.stage.Stage;
 public class AnswersApplication extends Application {
 
     private static final Logger LOG = Logger.getLogger(AnswersApplication.class.getName());
-    public static final ResourceBundle MESSAGES = ResourceBundle.getBundle("i18n/messages", Locale.ITALIAN);
-    public static final ResourceBundle LOG_MESSAGES = ResourceBundle.getBundle("i18n/log_messages", Locale.ITALIAN);
+    public static final ResourceBundle MESSAGES = ResourceBundle.getBundle("i18n/messages", Locale.getDefault());
+    public static final ResourceBundle LOG_MESSAGES = ResourceBundle.getBundle("i18n/log_messages", Locale.getDefault());
     private Thread listeningThread;
 
     public static void main(String[] args) {
