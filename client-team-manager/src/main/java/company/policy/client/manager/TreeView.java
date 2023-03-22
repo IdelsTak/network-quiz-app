6af7993bd2851d39213c1fb5a3632495cf1e2a8d
Hiram K <hiram.kamau@outlook.com>
2023-03-22 03:02:54 +0300

    Create TreeView.java

---
+++ client-team-manager/src/main/java/company/policy/client/manager/TreeView.java
@@ -0,0 +1,53 @@
+package company.policy.client.manager;
+
+import java.io.IOException;
+import java.util.Optional;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.TextArea;
+
+class TreeView {
+
+    private final Parent root;
+    @FXML
+    private TextArea treeTextArea;
+    private final String tree;
+
+    TreeView(String tree) {
+        this.tree = tree;
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("tree-pane.fxml"));
+        fxmlLoader.setController(this);
+        fxmlLoader.setResources(ResourceBundles.ITALIAN.getResourceBundle());
+        try {
+            root = (Parent) fxmlLoader.load();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @FXML
+    protected void initialize() {
+        treeTextArea.setText(tree);
+    }
+
+    Optional<ButtonType> showDialog() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+
+        dialog.setDialogPane((DialogPane) root);
+
+        dialog.setResultConverter(bt -> {
+            if (bt == ButtonType.OK) {
+                return bt;
+            }
+            return null;
+        });
+        
+        return dialog.showAndWait();
+    }
+
+}
