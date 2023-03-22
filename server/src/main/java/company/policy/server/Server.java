85df0b0865cbc93ae92f6b3a5d9e3356334dde64
Hiram K <hiram.kamau@outlook.com>
2023-03-22 10:21:18 +0300

    Update Server.java

--- server/src/main/java/company/policy/server/Server.java
+++ server/src/main/java/company/policy/server/Server.java
@@ -20,6 +20,55 @@ public class Server {
 
     private static final Logger LOG = Logger.getLogger(Server.class.getName());
 
+//    public static void main(String[] args) throws Exception {
+//        int port = 8080;
+//
+//        ServerSocket serverSocket = new ServerSocket(port);
+//        LOG.log(Level.INFO, "Server started on port {0}", serverSocket);
+//
+//        ExecutorService executorService = Executors.newFixedThreadPool(100);
+//        Map<UUID, Socket> map = new ConcurrentHashMap<>();
+//
+////        new Thread(() -> {
+//            while (true) {
+//                try {
+//                    Socket socket = serverSocket.accept();
+//
+//                    LOG.log(Level.INFO, "Connected: {0}", socket);
+//
+//                    UUID id = UUID.randomUUID();
+//
+//                    map.put(id, socket);
+//
+//                    executorService.submit(() -> {
+//                            try (ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+//
+//                                Object read = in.readObject();
+//
+//                                LOG.log(Level.INFO, "Read: {0}", read);
+//
+//                                List<Socket> sockets = map.entrySet().stream().filter(e -> !Objects.equals(e.getKey(), id)).map(Entry::getValue).toList();
+//
+//                                synchronized (sockets) {
+//                                    for (Socket otherSocket : sockets) {
+//                                        try (ObjectOutputStream out = new ObjectOutputStream(otherSocket.getOutputStream())) {
+//                                            out.writeObject(read);
+//                                        }
+//                                    }
+//
+//                                    map.clear();
+//                                }
+//                            } catch (IOException | ClassNotFoundException ex) {
+//                                LOG.log(Level.SEVERE, null, ex);
+//                            }
+//                    });
+//                } catch (IOException ex) {
+//                    LOG.log(Level.SEVERE, null, ex);
+//                }
+//            }
+////        }).start();
+//
+//    }
     public static void main(String[] args) throws Exception {
         int port = 8080;
 
@@ -29,45 +78,52 @@ public class Server {
         ExecutorService executorService = Executors.newFixedThreadPool(100);
         Map<UUID, Socket> map = new ConcurrentHashMap<>();
 
-        new Thread(() -> {
-            while (true) {
-                try {
-                    Socket socket = serverSocket.accept();
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
 
-                    LOG.log(Level.INFO, "Connected: {0}", socket);
+                LOG.log(Level.INFO, "Connected: {0}", socket);
 
-                    UUID id = UUID.randomUUID();
+                UUID id = UUID.randomUUID();
 
-                    map.put(id, socket);
+                map.put(id, socket);
 
-                    executorService.submit(() -> {
-                        try (ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+                executorService.submit(() -> {
+                    try (ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+                        Object read = in.readObject();
 
-                            Object read = in.readObject();
+                        LOG.log(Level.INFO, "Read: {0}", read);
 
-                            LOG.log(Level.INFO, "Read: {0}", read);
+                        List<Socket> sockets = map.entrySet().stream()
+                                .filter(e -> !Objects.equals(e.getKey(), id))
+                                .map(Entry::getValue)
+                                .toList();
 
-                            List<Socket> sockets = map.entrySet().stream().filter(e -> !Objects.equals(e.getKey(), id)).map(Entry::getValue).toList();
-
-                            synchronized (sockets) {
-                                for (Socket otherSocket : sockets) {
-                                    try (ObjectOutputStream out = new ObjectOutputStream(otherSocket.getOutputStream())) {
-                                        out.writeObject(read);
-                                    }
-                                }
-                                
-                                map.clear();
+                        for (Socket otherSocket : sockets) {
+                            try {
+                                ObjectOutputStream out = new ObjectOutputStream(otherSocket.getOutputStream());
+                                out.writeObject(read);
+                                out.flush();
+                            } catch (IOException ex) {
+                                LOG.log(Level.SEVERE, null, ex);
                             }
-                        } catch (IOException | ClassNotFoundException ex) {
+                        }
+
+                        map.remove(id);
+                    } catch (IOException | ClassNotFoundException ex) {
+                        LOG.log(Level.SEVERE, null, ex);
+                    } finally {
+                        try {
+                            socket.close();
+                        } catch (IOException ex) {
                             LOG.log(Level.SEVERE, null, ex);
                         }
-                    });
-                } catch (IOException ex) {
-                    LOG.log(Level.SEVERE, null, ex);
-                }
+                    }
+                });
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, null, ex);
             }
-        }).start();
-
+        }
     }
 
 }
